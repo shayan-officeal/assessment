61124f5509116b34be80c0077f06f52377875970
Shayan <shayan.officeal@gmail.com>
2026-02-03 05:28:44 +0500

    chore: database configurations

--- Assessment/settings.py
+++ Assessment/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'Assessment.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "assessment",
+        "USER": "root",
+        "PASSWORD": "admin",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
